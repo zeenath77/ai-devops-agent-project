fcc253631598bb1e0e28071f5cba90f0d89e1398
Syeda zeenath uzma <zeenathuzma62811@gmail.com>
2026-03-13 15:46:30 +0530

    trigger pipeline

--- ai-devops-agent-main/terraform-review-agent/terraform/security.tf
+++ ai-devops-agent-main/terraform-review-agent/terraform/security.tf
@@ -50,6 +50,7 @@ resource "aws_vpc_security_group_ingress_rule" "mario_alb_sg_https" {
 }
 */
 # test AI pipeline
+# pipeline test
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic" {
   security_group_id = aws_security_group.mario_alb_sg.id
   cidr_ipv4         = "0.0.0.0/0"
